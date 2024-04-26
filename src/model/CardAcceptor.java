8dd4e7c86cbd40db4a17b2ab0e078c27c6fef775
DaniyarAA <gitmailhub@gmail.com>
2024-04-26 15:49:04 +0600

    В классе CardAcceptor добавил метод insertCard для ввода карты, и вызвал в конструкторе

--- src/model/CardAcceptor.java
+++ src/model/CardAcceptor.java
@@ -1,2 +1,32 @@
-package model;public class CardAcceptor {
+package model;
+
+import interfaces.MoneyAcceptable;
+
+import java.util.Scanner;
+
+public class CardAcceptor implements MoneyAcceptable {
+    public int amount;
+    public String cardNumber;
+    public String oneTimePassword;
+    public CardAcceptor(int amount){
+        this.amount = amount;
+        insertCard();
+    }
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    public void insertCard() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите номер вашей банковской карты: ");
+        this.cardNumber = scanner.nextLine();
+        System.out.print("Введите одноразовый пароль: ");
+        this.oneTimePassword = scanner.nextLine();
+        System.out.print("Карта принята. ");
+    }
 }
